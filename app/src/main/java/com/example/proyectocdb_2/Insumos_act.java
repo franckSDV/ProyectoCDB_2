1d03f331c42f205aafc7fe2288c1552df705cf51
franckSDV <fgarciahu@gmail.com>
2021-11-16 18:24:10 -0300

    Primer commit

---
+++ app/src/main/java/com/example/proyectocdb_2/Insumos_act.java
@@ -0,0 +1,14 @@
+package com.example.proyectocdb_2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Insumos_act extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_insumos);
+    }
+}
